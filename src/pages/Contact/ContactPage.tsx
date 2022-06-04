e7dd6137655a551216c78a00cacf35f889da6053
KevinGrant-dev <kevingrant.0301@gmail.com>
2022-06-04 15:46:02 -0400

    setup enviroment

---
+++ src/pages/Contact/ContactPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './ContactPage.module.scss'
+
+const ContactPage = () => {
+  return (
+    <div className={styles.contactpage}>
+      <h1>This is contact page</h1>
+    </div>
+  )
+}
+
+export default ContactPage
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './HomePage.module.scss'
+
+const HomePage = () => {
+  return (
+    <div className={styles.homepaeg}>
+      <h1>This is home page</h1>
+    </div>
+  )
+}
+
+export default HomePage
