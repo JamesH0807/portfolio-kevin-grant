dcfea22f6d762ddfcfea962931d409ba0bcba6c6
KevinGrant-dev <kevingrant.0301@gmail.com>
2022-06-05 20:26:01 -0400

    compete contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 import Header from './layouts/Header'
 import Contact from './pages/Contact'
@@ -9,13 +10,21 @@ import Home from './pages/Home'
 import './App.scss'
 
 function App() {
+  const location = useLocation()
+
   return (
     <div className='App'>
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <main>
+        <TransitionGroup>
+          <CSSTransition timeout={400} classNames='fade' key={location.key}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/contact' element={<Contact />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </main>
     </div>
   )
 }
--- src/pages/Contact/ContactPage.tsx
+++ src/pages/Contact/ContactPage.tsx
@@ -2,10 +2,57 @@ import React from 'react'
 
 import styles from './ContactPage.module.scss'
 
+const contactInfo = [
+  {
+    icon: 'fa fa-envelope',
+    title: 'Email Address',
+    content: 'kevingrant.0301@gmail.com',
+  },
+  {
+    icon: 'fa fa-phone',
+    title: 'Phone Number',
+    content: '+1 (727) 266-6677',
+  },
+  {
+    icon: 'fa fa-linkedin',
+    title: 'Linkedin',
+    content: 'linkedin.com/in/kevin-grant-25176123b',
+  },
+  {
+    icon: 'fa fa-github',
+    title: 'Github Profile',
+    content: 'github.com/KevinGrant-dev',
+  },
+  {
+    icon: 'fa fa-telegram',
+    title: 'Telegram',
+    content: 't.me/KevinGrant0301',
+  },
+  {
+    icon: 'fa fa-skype',
+    title: 'Skype ID',
+    content: 'live:.cid.946f12e8c5a71b06',
+  },
+]
+
 const ContactPage = () => {
   return (
     <div className={styles.contactpage}>
-      <h1>This is contact page</h1>
+      <div className='container'>
+        <h1>CONTACT ME</h1>
+        <h2>Always open to work (remote preferred)</h2>
+        <div className='contact-item'>
+          {contactInfo.map((item, index) => (
+            <div className='item' key={index}>
+                <i className={item.icon}></i>
+              <div className='info'>
+                <h3 className='info-title'>{item.title}</h3>
+                <p className='info-content'>{item.content}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
